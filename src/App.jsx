06ac7c42ff115d1bfa22a4ea1bcd59ed886bd928
Hitendra singh <hitendra369432@gmail.com>
2025-01-19 15:28:07 +0530

    fixed import coding -> Coding

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import About from "./Components/About/About"
 import Projects from "./Components/Projects/Projects"
-import Coding from "./Components/Coding/coding"
+import Coding from "./Components/Coding/Coding"
 import BottomNavBar from "./Components/Navbar/BottomNavBar"
 import WorkExperience from "./Components/WorkExperience/WorkExperience"
 import Contact from "./Components/Contact/Contact"
